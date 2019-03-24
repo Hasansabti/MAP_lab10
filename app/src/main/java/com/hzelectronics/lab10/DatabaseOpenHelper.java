a5daf82c7549cf10cdd972300581f84798e01897
Hasanalsabti <7snsabti@gmail.com>
2019-03-24 11:52:31 +0300

    worked

---
+++ app/src/main/java/com/hzelectronics/lab10/DatabaseOpenHelper.java
@@ -0,0 +1,13 @@
+package com.hzelectronics.lab10;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DatabaseOpenHelper extends SQLiteAssetHelper {
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, "lab10", null, null, 1);
+    }
+}
--- app/src/main/java/com/hzelectronics/lab10/MainActivity.java
+++ app/src/main/java/com/hzelectronics/lab10/MainActivity.java
@@ -1,13 +1,42 @@
 package com.hzelectronics.lab10;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+EditText courseName, courseCode;
+Spinner day;
+TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        courseName = findViewById(R.id.editText);
+        courseCode = findViewById(R.id.editText2);
+        day = findViewById(R.id.spinner);
+        timePicker = findViewById(R.id.timePicker);
+
+
+
+    }
+
+    public void addCourse(View v){
+
+String cName courseName.getText().toString();
+
+String cCode = courseCode.getText().toString();
+String selectedday = day.getSelectedItem().toString();
+int hour = timePicker.getHour();
+int minutes = timePicker.getMinute();
+DatabaseOpenHelper dbh = new DatabaseOpenHelper(this);
+
+        ContentValues
+
     }
 }
